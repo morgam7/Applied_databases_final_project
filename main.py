5de53129f903439f72d1ed28f1f1c8471c7a70bd
Marcella Morgan <conactmarcellamorgan@gmail.com>
2026-04-19 23:10:47 +0100

    Working on displaying main menu and speaker name

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ conn = mysql.connector.connect(
 )
 
 print("Connected")
-
+'''
 cursor = conn.cursor()
 cursor.execute("SHOW TABLES")
 
@@ -17,3 +17,42 @@ for table in cursor:
 
 cursor.close()
 conn.close()
+'''
+
+cursor = conn.cursor()
+
+print("Conference Management")
+print("---------------------")
+print("Menu") 
+print("----")
+print("----")
+print("1 - View Speakers & Sessions")
+print("2 - View Attendees by Company")
+print("3 - Add New Attendee")
+
+choice = input("Choice:")
+
+if choice == "1":
+
+    input = (input("Enter speaker name :"))
+    
+    query = """
+    SELECT speakerName 
+    FROM session 
+    WHERE speakerName LIKE %s
+    """
+    cursor.execute(query, ('%' + input + '%',))
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+else:
+    print("Wrong choice")
+
+
+
+
+cursor.close()
+conn.close()
+
